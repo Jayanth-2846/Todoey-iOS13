70ed9cb4405d8de6fc6287d6b609be3e565a7c79
Jayanth <jayanthambaldhage15@gmail.com>
2023-04-25 00:28:55 +0530

    implement CRUD in Realm

--- Todoey/view/CategoryViewController.swift
+++ Todoey/view/CategoryViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
-import CoreData
+import RealmSwift
 class CategoryViewController: UITableViewController {
-    var categoryArray = [Category]()
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let realm = try! Realm()
+    var categories: Results<Category>?
+//    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     override func viewDidLoad() {
         super.viewDidLoad()
         loadCategories()
@@ -20,14 +22,14 @@ class CategoryViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return categoryArray.count
+        return categories?.count ?? 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        let item = categoryArray[indexPath.row]
-        cell.textLabel?.text = item.name
+        cell.textLabel?.text =  categories?[indexPath.row].name ?? "No categories added yet"
+//        cell.textLabel?.text = item.name
 //        value = condition ? valueifTrue: valueifFalse
         return cell
     }
@@ -37,11 +39,12 @@ class CategoryViewController: UITableViewController {
         
         var textField = UITextField()
         let alert = UIAlertController(title: "Add new category", message: "", preferredStyle: .alert)
+        
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
-            let newItem = Category(context: self.context)
-            newItem.name = textField.text!
-            self.categoryArray.append(newItem)
-            self.saveCategories()
+            let newCategory = Category()
+            newCategory.name = textField.text!
+//            self.categories.append(newCategory)
+            self.save(category: newCategory)
         }
         
         alert.addTextField {
@@ -63,36 +66,41 @@ class CategoryViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationVC = segue.destination as! TodoListViewController
         if let indexPath = tableView.indexPathForSelectedRow {
-            destinationVC.selectedCategory = categoryArray[indexPath.row]
+            destinationVC.selectedCategory = categories?[indexPath.row]
         }
     }
     
     
     //MARK: - Data manipulation methods
-    func saveCategories() {
+    func save(category: Category) {
         
 //        let encoder = PropertyListEncoder()
         do {
-//            let data = try encoder.encode(self.itemArray)
+//            let data = try encoder.encode(self.todoItems)
 //            try data.write(to: self.dataFilePath!)
-           try context.save()
+//           try context.save()
+            try realm.write {
+                realm.add(category)
+            }
         } catch {
             print("Error encoding item array, \(error)")
             print("Error saving context, \(error)")
         }
-//            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+//            self.defaults.set(self.todoItems, forKey: "TodoListArray")
         self.tableView.reloadData()
     }
     
-    func loadCategories(with request : NSFetchRequest<Category> = Category.fetchRequest()) {
+    func loadCategories() {
         
+         categories = realm.objects(Category.self)
+//        with request : NSFetchRequest<Category> = Category.fetchRequest()
 //        let request : NSFetchRequest<Item> = Item.fetchRequest()
-        do {
-            categoryArray = try context.fetch(request)
-        } catch {
-            print("Error fetching data from context,\(error)")
-        }
-        tableView.reloadData()
+//        do {
+//            categories = try context.fetch(request)
+//        } catch {
+//            print("Error fetching data from context,\(error)")
+//        }
+//        tableView.reloadData()
     }
 
 }
